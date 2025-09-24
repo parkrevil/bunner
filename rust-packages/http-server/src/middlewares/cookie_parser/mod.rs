829f845b46a3d62c2c999993b2ffddb30a28b767
revil <revil.com@gmail.com>
2025-09-25 03:11:36 +0900

    refactor: modularize cookie parser

---
+++ rust-packages/http-server/src/middlewares/cookie_parser/mod.rs
@@ -0,0 +1,28 @@
+mod parser;
+
+use crate::structures::{BunnerRequest, BunnerResponse, HandleRequestPayload};
+use parser::parse_cookie_header;
+use serde_json::json;
+
+use super::Middleware;
+
+pub struct CookieParser;
+
+impl Middleware for CookieParser {
+    #[tracing::instrument(level = "trace", skip(self, req, _res, _payload))]
+    fn handle(
+        &self,
+        req: &mut BunnerRequest,
+        _res: &mut BunnerResponse,
+        _payload: &HandleRequestPayload,
+    ) -> bool {
+        tracing::event!(tracing::Level::TRACE, operation = "cookie_parser");
+
+        if let Some(cookie_header) = req.headers.get("cookie") {
+            let parsed = parse_cookie_header(cookie_header);
+            req.cookies = json!(parsed);
+        }
+
+        true
+    }
+}
